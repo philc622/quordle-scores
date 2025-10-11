c4612a2179d75a3c7fa129dfb859e82dca6f8ea8
philc622 <phil.capon@fastmail.com>
2025-10-11 18:33:16 +0100

    Update update_score.sh

--- update_score.sh
+++ update_score.sh
@@ -14,5 +14,7 @@ python ~/scripts/email-reader-with-sqlite.py ~/data/quordle_scores.db
 python ~/scripts/extract_to_csv.py ~/data/quordle_scores.db ~/data/quordle_scores.csv
 
 cp ~/data/quordle_scores.csv /var/www/html/quordle
+mkdir -p /var/www/html/quordle_data
+cp ~/data/quordle_scores.csv /var/www/html/quordle_data/quordle_scores.csv
 
 echo "Done!"
