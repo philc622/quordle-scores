ae37f31c295f468ce4391c2bbfa3b875ea9eb6b2
philc622 <phil.capon@fastmail.com>
2026-07-05 22:37:48 +0100

    Update update_scripts.sh

--- update_scripts.sh
+++ update_scripts.sh
@@ -31,7 +31,7 @@ echo "Copying Bash scripts..."
 cp quordle-scores/*.sh $SCRIPTS_FOLDER
 
 # echo "Copying html file"
-cp quordle-scores/docs/index.html /home/phil/nginx-pi/html
+cp quordle-scores/docs/index.html /var/www/html
 
 # Count copied files
 COPIED_COUNT=$(find "$SCRIPTS_FOLDER" -name "*.py" | wc -l)
